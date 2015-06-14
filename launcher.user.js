f985044cfe7e5c4d99aa24a3a61e4c6709913751
OmedY <OmedY@users.noreply.github.com>
2015-06-14 21:42:32 +0200

    launcher.user.js
    Agar.io BOT

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "FeedMePLS",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
